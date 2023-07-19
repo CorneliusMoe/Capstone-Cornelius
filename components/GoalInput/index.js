0e62eaf0b30ec3172fc61931e3f7329098687884
Cornelius Mörig <c.moerig@gmail.com>
2023-07-19 11:13:05 +0200

    delete unnecessary code

--- components/GoalInput/index.js
+++ components/GoalInput/index.js
@@ -7,7 +7,6 @@ export default function GoalInput({
   onChange,
   timelyOption,
   setTimelyOption,
-  required,
 }) {
   function handleTimelyOptionChange(event) {
     const { value } = event.target;
